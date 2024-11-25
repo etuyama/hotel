1476b3f5106cd9f3e1c190990feb67aa93f1e42d
etuyama <egerassi@gmail.com>
2024-11-25 20:05:58 -0300

    Adiciona tela do cliente

--- limite/tela_cliente.py
+++ limite/tela_cliente.py
@@ -1,46 +1,99 @@
 from limite.tela import Tela
-
+import PySimpleGUI as sg
 
 class TelaCliente(Tela):
 
-    def tela_opcoes(self):
-        escolhas = [1,2,3,4,0]
-        print("-------- CLIENTES ----------")
-        print("Escolha uma opção")
-        print("1 - Incluir Cliente")
-        print("2 - Alterar Cliente")
-        print("3 - Listar Clientes")
-        print("4 - Excluir Cliente")
-        print("0 - Retornar")
+    def __init__(self):
+        super().__init__()
+        self.__window = None
+        self.init_opcoes()
 
-        escolha = super().le_num_inteiro("Escolha: ", escolhas)
-        return escolha
+    def tela_opcoes(self):
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['0'] or button in (None, 'Cancelar'):
+            opcao = 0
+        self.close()
+        return opcao
+
+    def init_opcoes(self):
+        sg.ChangeLookAndFeel('Dark')
+        layout = [
+            [sg.Text('Clientes', font=('Helvetica', 25))],
+            [sg.Text('Escolha sua opção', font=('Helvetica', 15))],
+            [sg.Radio('Incluir Cliente', 'C01', key='1')],
+            [sg.Radio('Alterar Cliente', 'C01', key='2')],
+            [sg.Radio('Listar Clientes', 'C01', key='3')],
+            [sg.Radio('Excluir Cliente', 'C01', key='4')],
+            [sg.Radio('Retornar', 'C01', key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Clientes').Layout(layout)
 
     def pega_dados_cliente(self):
-        print("-------- DADOS CLIENTE --------")
-        #Sem try except aqui pois exceções estão sendo tratadas na leitura
-        nome = super().le_string("Nome: ")
-        cpf = super().le_cpf("CPF: ")
-        data_nascimento = super().le_data(
-            "Data de nascimento (DD/MM/AAAA): "
-        )
-        telefone = super().le_telefone(
-            "Telefone (Formato: DDD123456789, DDD com 0 na frente): "
-        )
-        endereco = super().le_string("Endereco: ")
+        sg.ChangeLookAndFeel('Dark')
+        layout = [
+            [sg.Text('Dados Cliente', font=("Helvica", 25))],
+            [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Text('Telefone:', size=(15, 1)), sg.InputText('', key='telefone')],
+            [sg.Text('Data de nascimento:', size=(15, 1)), sg.InputText('', key='data_nascimento')],
+            [sg.Text('Endereço:', size=(15, 1)), sg.InputText('', key='endereco')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema do Hotel').Layout(layout)
+
+        button, values = self.open()
+        nome = values['nome']
+        cpf = values['cpf']
+        telefone = values['telefone']
+        data_nascimento = values['data_nascimento']
+        endereco = values['endereco']
+
+        self.close()
         return {"nome": nome, "cpf": cpf, "telefone": telefone,
                 "data_nascimento": data_nascimento, "endereco": endereco}
 
 
     def mostra_cliente(self, dados_cliente):
-        print("Nome do cliente: ", dados_cliente["nome"])
-        print("CPF: ", dados_cliente["cpf"])
-        print("Data de nascimento: ", dados_cliente["data_nascimento"])
-        print("Telefone: ", dados_cliente["telefone"])
-        print("Endereco: ", dados_cliente["endereco"])
-        print("\n")
+        string_todos_clientes = ""
+        string_todos_clientes = string_todos_clientes + "Nome do Cliente: " + dados_cliente["nome"] + '\n'
+        string_todos_clientes = string_todos_clientes + "CPF do Cliente: " + dados_cliente["cpf"] + '\n'
+        string_todos_clientes = string_todos_clientes + "Telefone do Cliente: " + dados_cliente["telefone"] + '\n'
+        string_todos_clientes = string_todos_clientes + "Data de nascimento do Cliente: " + str(dados_cliente["data_nascimento"]) + '\n'
+        string_todos_clientes = string_todos_clientes + "Endereço do Cliente: " + dados_cliente["endereco"] + '\n\n'
+
+        sg.Popup('Lista de Clientes', string_todos_clientes)
 
     def seleciona_cliente(self):
-        cpf = input("CPF do cliente que deseja selecionar: ")
+        sg.ChangeLookAndFeel('Dark')
+        layout = [
+            [sg.Text('Selecionar Cliente', font=("Helvica", 25))],
+            [sg.Text('Digite o CPF do Cliente que deseja selecionar:', font=("Helvica", 15))],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Seleciona cliente').Layout(layout)
+
+        button, values = self.open()
+        cpf = values['cpf']
+        self.close()
         return cpf
 
+    def mostra_mensagem(self, mensagem):
+        sg.Popup('', mensagem)
+
+    def close(self):
+        self.__window.close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -13,17 +13,17 @@ class TelaSistema(Tela):
         opcao = 0
         if values['1']:
             opcao = 1
-        if values['2']:
+        elif values['2']:
             opcao = 2
-        if values['3']:
+        elif values['3']:
             opcao = 3
-        if values['4']:
+        elif values['4']:
             opcao = 4
-        if values['5']:
+        elif values['5']:
             opcao = 5
-        if values['6']:
+        elif values['6']:
             opcao = 6
-        if values['0'] or button in (None,'Cancelar'):
+        elif values['0'] or button in (None,'Cancelar'):
             opcao = 0
         self.close()
         return opcao
@@ -42,7 +42,7 @@ class TelaSistema(Tela):
             [sg.Radio('Quartos', "S01", key='4')],
             [sg.Radio('Funcionários', "S01", key='5')],
             [sg.Radio('Hotel ', "S01", key='6')],
-            sg.Radio('Finalizar Sistema', "S01", key='0')
+            [sg.Radio('Finalizar Sistema', "S01", key='0')],
             [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
         ]
         self.__window = sg.Window('Sistema de Hotel').Layout(layout)
